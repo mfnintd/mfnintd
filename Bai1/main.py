aa0329855a5769fcee636dbf585ef3d2e2f0868f
mfnintd <80694782+mfnintd@users.noreply.github.com>
2021-09-13 15:43:11 +0700

    Add files via upload

---
+++ Bai1/main.py
@@ -0,0 +1,10 @@
+from PhuongTrinhBac2 import phuongtrinhbac2
+
+object1 = phuongtrinhbac2(0, 0, 0)
+
+object2 = phuongtrinhbac2()
+object2.nhap()
+object2.xuat()
+object2.giai()
+
+del object1, object2
